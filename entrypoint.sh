928d7564ad176eb802d54ca4269000fb7fab5cd4
Rodrigo Rivera <rodrigo.rivera@jaak-it.com>
2019-04-04 16:54:30 -0600

    Change entrypoint so that docker keeps running

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,6 @@ set -e
 
 /opt/gitlab/embedded/bin/runsvdir-start & gitlab-ctl reconfigure
 #EXTERNAL_URL="http://gitlab.fiinlab.jaak-it.com" yum install -y gitlab-ce
+while true; do sleep 1; done
 
 exec "$@"
